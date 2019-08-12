b6774df72854d9193868d4f65cff2c6e8b611e98
thanks124 <53394936+thanks124@users.noreply.github.com>
2019-08-12 14:41:52 +0530

    Create mass.py

---
+++ mass.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext.commands import bot
+from discord import game
+from discord.ext import commands
+import asyncio
+import platform
+import colorsys
+import random
+import time
+
+client = commands.Bot(command_prefix = '+', case_insensitive=True)
+Client = discord.client
+Clientdiscord = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('--------')
+    print('Created by niloj')
+
+
+@commands.has_permissions(administrator=True)
+@client.command(pass_context = True)
+async def send(ctx, *, content: str):
+        for member in ctx.message.server.members:
+            try:
+                await client.send_message(member, content)
+                await client.say("DM Sent To : {} :white_check_mark:  ".format(member))
+            except:
+                print("can't")
+                await client.say("DM can't Sent To : {} :x: ".format(member))
+
+
+client.run("NjA0NTE1NDA2NzY5Njg0NTIy.XU_37Q.2cwLVPya-auSO7c_0xPSwjJaxC8")
